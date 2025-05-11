3a3b256dcedcca5f44eb53b33541a0a972e6d97c
Utkarsh Upadhyay <utkarshup562@gmail.com>
2025-05-11 16:20:55 +0530

    Delete backend directory

--- backend/routes/reports.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-router.get('/', reportController.getVaccinationReport);
-router.get('/download', reportController.downloadVaccinationReport);
-
-module.exports = router;
